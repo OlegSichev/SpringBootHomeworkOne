83c79eb8e0515c0b14e24f95434741c6e31dee75
Oleg Sichev <sichev.oleg@bk.ru>
2023-09-25 13:48:03 +0300

    fix

--- src/main/java/oleg/sichev/springboothomeworkone/JavaConfig.java
+++ src/main/java/oleg/sichev/springboothomeworkone/JavaConfig.java
@@ -2,15 +2,18 @@ package oleg.sichev.springboothomeworkone;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 
 public class JavaConfig {
-    @ConditionalOnProperty
+    @ConditionalOnProperty(value = "netology.profile.dev", havingValue = "true", matchIfMissing = true)
     @Bean
     public SystemProfile devProfile(){
         return new DevProfile();
     }
 
-    @ConditionalOnProperty
+    @ConditionalOnProperty(value = "netology.profile.dev", havingValue = "false")
     @Bean
     public SystemProfile prodProfile(){
         return new ProductionProfile();
